534cb10f822a01bc8c7fda72b56ea1df1c75a02c
Gregory Colella <gcolella@Gregorys-MBP.home>
2015-04-25 16:14:44 -0400

    Added dependencies to pyparsing

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 setup(name='pseudo',
 	version='1.2',
 	packages=['pseudo'],
+	install_requires = 'pyparsing>=2.0.1',
  	entry_points = {
               'console_scripts': [
                   'pseudo=pseudo.main:main'                  
